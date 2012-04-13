005211efd8d91fe54806fc13265e9b961e0cc8e7
Franklin <fr@nkl.in>
2012-04-13 22:22:42 +0200

    Ignore invalid input to prevent server quit

--- tk102.js
+++ tk102.js
@@ -43,36 +43,41 @@ function tk102( raw ) {
 	// 1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123
 	var raw = raw.replace( /(^[\s\t\r\n]+|[\s\t\r\n]+$)/, '' );
 	var str = raw.split(',');
+	var data = {};
 	
-	// parse
-	var datetime = str[0].replace( /([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, year, month, day, hour, minute ) {
-		return '20'+ year +'-'+ month +'-'+ day +' '+ hour +':'+ minute
-	});
-	var gpsdate = str[11].replace( /([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, day, month, year ) {
-		return '20'+ year +'-'+ month +'-'+ day;
-	});
-	var gpstime = str[3].replace( /([0-9]{2})([0-9]{2})([0-9]{2})\.([0-9]{3})/, function( match, hour, minute, second, ms ) {
-		var hour = str[4] == 'V' ? hour + 12 : hour;
-		return hour +':'+ minute +':'+ second +'.'+ ms;
-	});
-	
-	var data = {
-		'raw':		raw,
-		'datetime':	datetime,
-		'phone':	str[1],
-		'gps': {
-			'date':		gpsdate,
-			'time':		gpstime,
-			'signal':	str[15]
-		},
-		'geo': {
-			'latitude':	parseFloat( fixGeo( str[5], str[6] ) ),
-			'longitude':	parseFloat( fixGeo( str[7], str[8] ) ),
-			'heading':	parseInt( str[10] )
-		},
-		'kmh':		parseFloat( str[9] ),
-		'imei':		str[16].replace( 'imei:', '' )
-	};
+	// only continue with correct input, else the server may quit...
+	if( raw.indexOf(',GPRMC,') && str.length == 18 ) {
+		
+		// parse
+		var datetime = str[0].replace( /([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, year, month, day, hour, minute ) {
+			return '20'+ year +'-'+ month +'-'+ day +' '+ hour +':'+ minute
+		});
+		var gpsdate = str[11].replace( /([0-9]{2})([0-9]{2})([0-9]{2})/, function( match, day, month, year ) {
+			return '20'+ year +'-'+ month +'-'+ day;
+		});
+		var gpstime = str[3].replace( /([0-9]{2})([0-9]{2})([0-9]{2})\.([0-9]{3})/, function( match, hour, minute, second, ms ) {
+			var hour = str[4] == 'V' ? hour + 12 : hour;
+			return hour +':'+ minute +':'+ second +'.'+ ms;
+		});
+		
+		data = {
+			'raw':		raw,
+			'datetime':	datetime,
+			'phone':	str[1],
+			'gps': {
+				'date':		gpsdate,
+				'time':		gpstime,
+				'signal':	str[15]
+			},
+			'geo': {
+				'latitude':	parseFloat( fixGeo( str[5], str[6] ) ),
+				'longitude':	parseFloat( fixGeo( str[7], str[8] ) ),
+				'heading':	parseInt( str[10] )
+			},
+			'kmh':		parseFloat( str[9] ),
+			'imei':		str[16].replace( 'imei:', '' )
+		};
+	}
 	
 	// done
 	return data;
