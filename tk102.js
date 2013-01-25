f303bd90417ef9eeb7d229f87d88fa7148bf5a5e
Franklin van de Meent <fr@nkl.in>
2013-01-25 23:36:04 +0100

    fixGeo: leading 0 causes octal radix instead of decimal, fixes #6

--- tk102.js
+++ tk102.js
@@ -145,7 +145,7 @@ tk102.parse = function( raw ) {
 // Clean geo positions, with 6 decimals
 tk102.fixGeo = function( one, two ) {
 	var minutes = one.substr(-7, 7)
-	var degrees = parseInt( one.replace( minutes, '' ) )
+	var degrees = parseInt( one.replace( minutes, '' ), 10 )
 	var one = degrees + (minutes / 60)
 
 	var one = parseFloat( (two == 'S' || two == 'W' ? '-' : '') + one )
