800cc1c40eab7331dfd76428a741300343396b8b
Franklin van de Meent <fr@nkl.in>
2013-01-25 23:47:12 +0100

    Changed license to Unlicense (public domain)
    This 'license' is more generic and contains a disclaimer.

--- tk102.js
+++ tk102.js
@@ -1,13 +1,36 @@
 /*
-TK102 GPS server for Node.js
+Name:         tk102
+Description:  TK102 GPS server for Node.js
+Source:       https://github.com/fvdm/nodejs-tk102
+Feedback:     https://github.com/fvdm/nodejs-tk102/issues
+License:      Unlicense / Public Domain
 
-License: COPYLEFT
-This code is released under COPYLEFT meaning you can do with it
-anything you like except copyrighting it. If possible it would
-be nice to include the source URL for future reference:
+This is free and unencumbered software released into the public domain.
 
-https://github.com/fvdm/nodejs-tk102
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
 */
+
 
 // INIT
 var net = require('net'),
