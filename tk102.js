d0a218fddbf2abf7550b72276e81b7edea899f2e
Franklin van de Meent <fr@nkl.in>
2013-05-01 23:05:54 +0200

    Fixed raw.indexOf

--- tk102.js
+++ tk102.js
@@ -118,7 +118,7 @@ tk102.parse = function( raw ) {
 	
 	var data = false
 	
-	if( raw.indexOf('GPRMC') ) {
+	if( raw.indexOf('GPRMC') > -1 ) {
 	
 		// TK102 old
 		// 1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123
