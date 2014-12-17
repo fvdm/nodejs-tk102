6e8eb6082cbdab994dfedf24cc60401c0c02137f
Franklin van de Meent <fr@nkl.in>
2014-12-17 12:28:45 +0100

    Replaced unlicense text with reference

--- tk102.js
+++ tk102.js
@@ -3,32 +3,8 @@ Name:         tk102
 Description:  TK102 GPS server for Node.js
 Source:       https://github.com/fvdm/nodejs-tk102
 Feedback:     https://github.com/fvdm/nodejs-tk102/issues
-License:      Unlicense / Public Domain
-
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org>
+License:      Unlicense / Public Domain (see UNLICENSE file)
+              (https://github.com/fvdm/nodejs-tk102/raw/master/UNLICENSE)
 */
 
 
