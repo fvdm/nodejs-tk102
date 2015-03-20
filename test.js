70b913421d70c5ef8cba4f26ececc27a7e5d0638
Franklin van de Meent <fr@nkl.in>
2015-03-20 19:47:56 +0100

    Test: modernized the code a bit

--- test.js
+++ test.js
@@ -1,72 +1,81 @@
-var app = require('./')
+/*
+Name:         tk102 - test.js
+Description:  Test script for TK102 GPS server for node.js
+Author:       Franklin van de Meent (https://frankl.in)
+Source:       https://github.com/fvdm/nodejs-tk102
+Feedback:     https://github.com/fvdm/nodejs-tk102/issues
+License:      Unlicense / Public Domain (see UNLICENSE file)
+              (https://github.com/fvdm/nodejs-tk102/raw/master/UNLICENSE)
+*/
 
+var app = require ('./');
 
 // handle exits
-var errors = 0
-process.on( 'exit', function() {
-  if( errors == 0 ) {
-    console.log('\n\033[1mDONE, no errors.\033[0m\n')
-    process.exit(0)
+var errors = 0;
+process.on ('exit', function () {
+  if (errors === 0) {
+    console.log ('\n\033[1mDONE, no errors.\033[0m\n');
+    process.exit (0);
   } else {
-    console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n')
-    process.exit(1)
+    console.log ('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n');
+    process.exit (1);
   }
-})
+});
 
 // prevent errors from killing the process
-process.on( 'uncaughtException', function( err ) {
-  console.log()
-  console.error( err.stack )
-  console.trace()
-  console.log()
-  errors++
-})
+process.on ('uncaughtException', function (err) {
+  console.log ();
+  console.error (err.stack);
+  console.trace ();
+  console.log ();
+  errors++;
+});
 
 // Queue to prevent flooding
-var queue = []
-var next = 0
+var queue = [];
+var next = 0;
 
-function doNext() {
-  next++
-  if( queue[next] ) {
-    queue[next]()
+function doNext () {
+  next++;
+  if (queue [next]) {
+    queue [next] ();
   }
 }
 
 // doTest( passErr, 'methods', [
 //   ['feeds', typeof feeds === 'object']
 // ])
-function doTest( err, label, tests ) {
-  if( err instanceof Error ) {
-    console.error( label +': \033[1m\033[31mERROR\033[0m\n' )
-    console.error( util.inspect(err, false, 10, true) )
-    console.log()
-    console.error( err.stack )
-    console.log()
-    errors++
+function doTest (err, label, tests) {
+  if (err instanceof Error) {
+    console.error (label +': \033[1m\033[31mERROR\033[0m\n');
+    console.error (util.inspect (err, {depth: 10, colors: true}));
+    console.log ();
+    console.error (err.stack);
+    console.log ();
+    errors++;
   } else {
-    var testErrors = []
-    tests.forEach( function( test ) {
-      if( test[1] !== true ) {
-        testErrors.push(test[0])
-        errors++
+    var testErrors = [];
+    tests.forEach (function (test) {
+      if (test [1] !== true) {
+        testErrors.push (test [0]);
+        errors++;
       }
-    })
+    });
 
-    if( testErrors.length == 0 ) {
-      console.log( label +': \033[1mok\033[0m' )
+    if (testErrors.length === 0) {
+      console.log (label +': \033[1mok\033[0m');
     } else {
-      console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' )
+      console.error (label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')');
     }
   }
 
-  doNext()
+  doNext ();
 }
 
 // parser valid
-queue.push( function() {
-  var data = app.parse('1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123')
-  doTest( data, 'parse valid', [
+queue.push (function () {
+  var data = app.parse ('1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123');
+  doTest (data, 'parse valid', [
     ['type', data instanceof Object],
     ['phone', data.phone === '0031698765432'],
     ['imei', data.imei === '123456789012345'],
@@ -82,17 +91,17 @@ queue.push( function() {
     ['geo.bearing', data.geo.bearing === 273],
     ['speed type', data.speed instanceof Object],
     ['speed.knots', data.speed.knots === 0]
-  ])
-})
+  ]);
+});
 
 // parser fail
-queue.push( function() {
-  var data = app.parse('invalid input')
-  doTest( data, 'parse fail', [
+queue.push (function () {
+  var data = app.parse ('invalid input');
+  doTest (data, 'parse fail', [
     ['type', data === null]
-  ])
-})
+  ]);
+});
 
 
 // Start the tests
-queue[0]()
+queue [0] ();
