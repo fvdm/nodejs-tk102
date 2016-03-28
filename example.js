8144c620abd0dddbf5839cf84cedc942285bc2a6
Franklin van de Meent <fvdm@users.noreply.github.com>
2016-03-28 16:28:42 +0200

    Minor comment edit in example.js

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 /*
-  Start with:  node demo.js
+  Start with:  node example.js
   Telnet to:   telnet 127.0.0.1 1337
   Copy/paste:  1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123
 */
